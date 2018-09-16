8f20222d8de334d615ee84fdefed76f5a0891d28
Eric Knorr <betrfs@bunsen9.cs.unc.edu>
2018-09-16 13:12:17 -0400

    reduced data set and fixed new line

--- lsm-benchmarks/run_rocksdb.sh
+++ lsm-benchmarks/run_rocksdb.sh
@@ -18,14 +18,14 @@ echo "Write Amp test" | tee -a "$log"
 echo "N_over_M, fanout, node_size, seq_write_amp, rand_write_amp" | tee -a "$log"
 
 for j in 4 8 16; do
-    for i in 268435456 134217728 67108864 33554432 16777216 8388608 4194304 2097152 1048576 524288; do
+    for i in 268435456 67108864 16777216 4194304 1048576; do
         if [ -d "$DB" ]; then
 	    	    rm -rf "$DB"/*
 	    fi
 
 	    #./$exe -l -b -s $i -k $keysize -v $valuesize | tee -a "$log"
 	    # rm -rf "$DB"/*
-	     ./$exe -r -f $j -s $i -k $keysize -v $valuesize | tee -a "$log"
+	     ./$exe -r -b -f $j -s $i -k $keysize -v $valuesize | tee -a "$log"
 	    echo | tee -a "$log"
     done
 done
